2f3c7ee75a9042ee2eac54330884dd44a25c904f
MaxNRG <maxnrg@gmail.com>
2018-07-24 14:54:52 +0300

    back_button_page -> page_back_button_url

--- person/forms.py
+++ person/forms.py
@@ -1,17 +1,33 @@
+from datetime import datetime
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
+from django.forms import SelectDateWidget, DateInput
+
 from person.models import Person
 
 
-class CalendarWidget(forms.TextInput):
-    class Media:
-        css = {
-            'all': ('pretty.css',),
-        }
-        js = ('animation.js', 'action.js')
+# class CalendarWidget(forms.TextInput):
+#     class Media:
+#         extend = False  # по умолчанию True
+#         css = {
+#             'all': ('pretty.css',)
+#         }
+#         js = ('animations.js', 'actions.js')
 
 
 class PersonForm(forms.ModelForm):
-    # test =
+    birthdate = forms.DateField(
+        widget=forms.SelectDateWidget(years=range(datetime.now().year - 100, datetime.now().year + 1)),
+        initial=datetime.now(),
+        # input_formats=('%d.%m.%Y', '%d/%m/%Y', '%d-%m-%Y'),
+    )
+
     class Meta:
         model = Person
         fields = '__all__'
+
+# class Media:
+#     css = {
+#         'all': ('form.css',)
+#     }
+#     js = ('form.js',)
