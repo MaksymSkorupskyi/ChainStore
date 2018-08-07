f23490284cf2f803e3ec62d264a1aab2f39cac6f
MaxNRG <maxnrg@gmail.com>
2018-08-07 11:36:57 +0300

    customizing Django Admin: Shops shows all fields now

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from shop.models import ShopType
 from shop.models import Shop
 
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = (
+        # '__str__',
+        'name',
+        'shop_type',
+        'owner',
+        # 'city.country',
+        'city',
+        'address',
+        'website',
+    )
+
+
 admin.site.register(ShopType)
-admin.site.register(Shop)
-# admin.site.register(ShopType, admin.ModelAdmin)
-# admin.site.register(shop, admin.ModelAdmin)
+admin.site.register(Shop, ShopAdmin)
